81619f8d48f152ecda4599575fa36216ba65125e
Aseem Sethi <aseemsethi@yahoo.com>
2016-09-27 16:41:43 +0530

    BGP TCP Connect and callback to activity
    BGP TCP Connect and callback to activity, with TCP connect status, bytes
    read etc.

--- app/src/main/java/in/netstack/netstacktool/bgp.java
+++ app/src/main/java/in/netstack/netstacktool/bgp.java
@@ -1,7 +1,9 @@
 package in.netstack.netstacktool;
 
 import android.app.Fragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +12,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.InetAddress;
+import java.net.Socket;
 
 //import android.support.v4.app.Fragment;
 
@@ -22,6 +31,8 @@ public class bgp extends Fragment{
     static final String SERVERIP = "172.217.26.206"; // this is from Saved State
     static final String GSERVERIP = "172.217.26.206";  // this is from Main Activity
     EditText bgp_server;
+    TextView bgp_report;
+    client myClient;
 
     String serverIP;
 
@@ -53,8 +64,9 @@ public class bgp extends Fragment{
         start_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d(TAG, "starting bgp server IP: " + serverIP);
-                connectToPeer(v, bgp_server.getText().toString(), 179, bgp_report);
+                Log.d(TAG, "starting bgp server IP: " + bgp_server.getText().toString());
+                myClient = new client(bgp_server.getText().toString(), 80, bgp_report); //179
+                myClient.execute();
             }
         });
         Button stop_button = (Button) v.findViewById(R.id.bgp_stop);
@@ -66,6 +78,26 @@ public class bgp extends Fragment{
             }
         });
         return v;
+    }
+
+    private void appendToOutput(String str) {
+        Log.d(TAG, "appendToOutput 2 called with: " + str);
+
+        bgp_report = (TextView) getActivity().findViewById(R.id.bgp_report);
+        bgp_report.append(str);
+        bgp_report.setMovementMethod(new ScrollingMovementMethod());
+    }
+
+    private void appendToOutput(byte[] data) {
+        String str;
+        Log.d(TAG, "appendToOutput 1 called with");
+        try {
+            str = new String(data, "UTF8");
+            appendToOutput(str);
+            appendToOutput("\n");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -81,12 +113,7 @@ public class bgp extends Fragment{
             Log.d(TAG, "!!!! Bundle is null");
         }
     }
-    private void connectToPeer(View v, String server, int port, TextView response) {
-        Log.d(TAG, "connecting to bgp server IP: " + server);
-        Toast.makeText(v.getContext(), "BGP connect to " + server, Toast.LENGTH_SHORT).show();
-        client myClient = new client(server, port, response);
-        myClient.execute();
-    }
+
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
         // Save the user's current game state
@@ -96,4 +123,81 @@ public class bgp extends Fragment{
         // Always call the superclass so it can save the view hierarchy state
         super.onSaveInstanceState(savedInstanceState);
     }
-}
+
+    public class client extends AsyncTask<String, byte[], Boolean> {
+        private static final String TAG = "BGP Client";
+        String dstAddress;
+        int dstPort;
+        String response = "";
+        boolean connected = false;
+        InetAddress in = null;
+        InputStream is;
+        OutputStream os;
+        byte buffer[] = new byte[30];
+
+        TextView textResponse;
+        client(String addr, int port, TextView response) {
+            dstAddress = addr;
+            dstPort = port;
+            this.textResponse = response;
+        }
+        protected Boolean doInBackground(String... arg0) {
+            Socket socket = null;
+            String Str1 = new String("Trying Connect");
+            String Str2 = new String("Connected");
+
+            try {
+                Log.d(TAG, "doInBackground called with: " + dstAddress);
+                System.arraycopy(Str1.getBytes("UTF-8"), 0, buffer, 0, Str1.length());
+                publishProgress(buffer);
+                socket = new Socket(dstAddress, dstPort);
+                Log.d(TAG, "socket connected");
+                System.arraycopy(Str2.getBytes("UTF-8"), 0, buffer, 0, Str2.length());
+                publishProgress(buffer);
+                is = socket.getInputStream();
+                os = socket.getOutputStream();
+                //This is blocking
+                int read;
+                while((read = is.read(buffer, 0, 30)) > 0 ) {
+                    byte[] idata = new byte[read];
+                    System.arraycopy(buffer, 0, idata, 0, read);
+                    publishProgress(idata);
+                    Log.i(TAG, "Recvd data bytes: " + read);
+                }
+            } catch (Exception e) {
+                Log.e("ClientActivity", "C: Error", e);
+                connected = false;
+                return false;
+            } finally {
+                try {
+                    is.close();
+                    os.close();
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                Log.d(TAG, "Finished");
+            }
+            return true;
+        }
+
+        @Override
+        protected void onProgressUpdate(byte[]... values) {
+            if (values.length > 0) {
+                Log.d(TAG, "onProgressUpdate: " + values[0].length + " bytes received.");
+                appendToOutput(buffer);
+            }
+        }
+        protected void onPostExecute(Boolean result) {
+            Log.d("ClientActivity", "onPostExecute called for " + result);
+            if (result == true)
+                textResponse.setText("Disconnected to BGP Port");
+            else
+                textResponse.setText("Disconnection Failed to BGP Port");
+
+            super.onPostExecute(result);
+        }
+    } // end async task class
+} // end activity
